7cdb618f9e7cd0e1ad7ca8695841062b2f2f7307
TusharRawat05 <tusharrawat9311@gmail.com>
2026-04-22 17:05:36 +0530

    updated the frontend url

--- src/features/Interview/services/interview.api.js
+++ src/features/Interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "https://ai-resume-analyse-backend.onrender.com",
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://backend-resume-analysed.onrender.com",
     withCredentials: true,
 })
 
